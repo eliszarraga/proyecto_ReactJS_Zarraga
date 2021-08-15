1170677064c539797cc352a70e6d3296f9b3191f
Elis Zarraga <elis.zarraga.valles@outlook.com>
2021-08-14 21:00:14 -0400

    actualizacion 14/08/2021 21:00

---
+++ src/componentes/Navbar/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export const Navbar = () => {
+
+    return(
+
+        <header className="encabezado">
+
+            <h1> HOLO! </h1>
+
+            <nav className="enlaces">
+
+                <a href="#">TODO</a>
+                <a href="#">ELECTRODOMESTICOS</a>
+                <a href="#">OFICINAS</a>
+                <a href="#">GAMERS</a>
+
+            </nav>
+
+        </header>
+
+    );
+}
